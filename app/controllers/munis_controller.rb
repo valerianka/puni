22c3927ed68ffe0cb83e404aa11715f89a057c03
Valerie Gorbik <valerianka@me.com>
2015-06-28 13:31:55 -0700

    Finish app

--- app/controllers/munis_controller.rb
+++ app/controllers/munis_controller.rb
@@ -1,15 +1,13 @@
 class MunisController < ApplicationController
 
     def index
-
     end
 
     def show
-        @muni = Muni.find_by(route_name: params[:route_name])
-        @muni.reports.build(smell_rating: params[:smell_rating], clean_rating: params[:clean_rating], driver_rating: params[:driver_rating])
-        @muni.update_avg_ratings
-        @muni.save
+        muni = Muni.find_by(route_name: params[:route_name]) || Muni.create(route_name: params[:route_name])
+        muni.reports.build(smell_rating: params[:smell_rating], clean_rating: params[:clean_rating], driver_rating: params[:driver_rating])
+        muni.update_avg_ratings
+        muni.save
+        @munis = Muni.sorted_munis
     end
-
-
 end
--- app/models/muni.rb
+++ app/models/muni.rb
@@ -1,6 +1,8 @@
 class Muni < ActiveRecord::Base
 
     has_many :reports
+    validates :route_name, uniqueness: true
+    before_save :default_avgs
 
     def update_avg_ratings
         num = 0
@@ -25,5 +27,22 @@ class Muni < ActiveRecord::Base
         end
         avg_driver_rating = sum / num
         update_attributes(avg_smelling_rating: avg_smell_rating, avg_clean_rating: avg_clean_rating, avg_driver_rating: avg_driver_rating)
+    end
+
+    class << self
+        def sorted_munis
+            munis = Muni.all 
+            munis.sort do |muni1, muni2| 
+                muni1_avg_sum = muni1.avg_smelling_rating + muni1.avg_clean_rating + muni1.avg_driver_rating
+                muni2_avg_sum = muni2.avg_smelling_rating + muni2.avg_clean_rating + muni2.avg_driver_rating
+                muni2_avg_sum - muni1_avg_sum
+            end
+        end
+    end
+
+    def default_avgs
+        avg_smelling_rating ||= 0
+        avg_clean_rating ||= 0
+        avg_driver_rating ||= 0
     end
 end
